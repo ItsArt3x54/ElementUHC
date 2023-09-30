693f875ae8397750a8f9f7fc2217b9885f1ef143
ItsArt3x54 <136159213+ItsArt3x54@users.noreply.github.com>
2023-09-30 12:13:55 +0200

    Added compos util file, added compos1.

--- src/main/java/be/artex/commands/MeetupCommand.java
+++ src/main/java/be/artex/commands/MeetupCommand.java
@@ -1,6 +1,7 @@
 package be.artex.commands;
 
 import be.artex.elementuhc.ElementUHC;
+import be.artex.utils.Compos;
 import be.artex.utils.ItemStacks;
 import org.bukkit.*;
 import org.bukkit.command.Command;
@@ -11,7 +12,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 
 public class MeetupCommand implements CommandExecutor {
@@ -66,13 +69,44 @@ public class MeetupCommand implements CommandExecutor {
                 p.getWorld().getWorldBorder().setCenter(0, 0);
                 p.getWorld().getWorldBorder().setSize(800);
 
-                Bukkit.getScheduler().runTaskLater(ElementUHC.PLUGIN, () -> {
-                    p.getWorld().setPVP(true);
-                    player.sendTitle(ChatColor.RED + "PVP IS ENABLED", ChatColor.RED + "PVP IS ON!");
-                }, 20 * 60);
                 player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 999999, 0));
 
             }
+
+            Bukkit.getScheduler().runTaskLater(ElementUHC.PLUGIN, () -> {
+
+                ArrayList<String> compos = Compos.compos1();
+
+                ArrayList<Player> solo = new ArrayList<>();
+                ArrayList<Player> eclair = new ArrayList<>();
+                ArrayList<Player> eau = new ArrayList<>();
+
+                for (Player player : players) {
+
+                    Random camps = new Random();
+                    int camps2 = camps.nextInt(compos.size() + 1);
+
+                    if (camps2 == 1) {
+                        solo.add(player);
+
+                        player.sendMessage("- Vous etes " +   ChatColor.GOLD + ChatColor.BOLD + "un etre corrompu.\n" + ChatColor.RESET + "- Vous devez gagner " + ChatColor.GOLD + ChatColor.BOLD + "Tous seul.");
+                    } else if (camps2 == 2) {
+                        eclair.add(player);
+
+                        player.sendMessage("Vous etes "  + ChatColor.YELLOW + ChatColor.BOLD + "un etre de l'eclair.\n" + ChatColor.RESET + "Vous devez gagner avec " + ChatColor.YELLOW + ChatColor.BOLD + "les membres du cercle de l'eclair");
+                    } else if (camps2 == 3){
+                        eau.add(player);
+
+                        player.sendMessage("Vous etes " +  ChatColor.BLUE + ChatColor.BOLD + "un etre de l'eau.\n" + ChatColor.RESET + "Vous devez gagner avec " + ChatColor.BLUE + ChatColor.BOLD + "les membres du cercle de l'eau");
+                    } else {
+
+                    }
+
+                    player.sendTitle(ChatColor.RED + "PVP IS ENABLED", ChatColor.RED + "PVP IS ON!");
+                }
+
+                p.getWorld().setPVP(true);
+            }, 20 * 60);
         }
         return true;
     }
---
+++ src/main/java/be/artex/utils/Compos.java
@@ -0,0 +1,18 @@
+package be.artex.utils;
+
+import java.util.ArrayList;
+
+public class Compos {
+
+    public static ArrayList<String> compos1() {
+        ArrayList<String> compos = new ArrayList<>();
+        compos.add("safe1");
+        compos.add("solo1");
+        compos.add("eau1");
+        compos.add("tndr1");
+
+        return compos;
+    }
+
+
+}
